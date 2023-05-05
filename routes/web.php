c542cdd38113b75d7d06d5cdb18c3952170c4ee9
Sekar Azalea <sekarazalea2003@gmail.com>
2023-05-05 22:21:38 +0700

    update2

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('homepage');
-// });
-
 Route::get('/',[HomepageController::class,'index'])->name('homepage');
